edafa7d192a25db63c3ef7a76735b920d8a6acd7
Anonymous Coder <cdroy36907@gmail.com>
2025-01-15 21:38:12 +0530

    3rd commits

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -33,10 +33,11 @@ export const internValidation = (req, res, next) => {
     });
 
     const { error } = schema.validate(req.body);
+    
     if (error) {
         return res.status(400).json({
             success: false,
-            message: 'Validation failed.',
+            message: error.details[0].message,
             error: error.details[0].message
         });
     }
